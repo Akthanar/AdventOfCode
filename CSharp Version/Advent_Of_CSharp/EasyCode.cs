b833fd282289d14e9b6aa5c98b4593983a0e271d
Akthanar <akthanar@gmail.com>
2023-12-11 19:25:59 +0100

    upload template

---
+++ CSharp Version/Advent_Of_CSharp/EasyCode.cs
@@ -0,0 +1,61 @@
+﻿namespace EasyCode
+{
+    internal class Input
+    {
+        // get an input from keyboard and return an object
+        public static object Get () => Console.ReadKey() switch
+        {
+            // convert in char then check if is a number
+            ConsoleKeyInfo k when char.IsNumber(k.KeyChar) => int.Parse(k.KeyChar.ToString()),
+            // convert in char then check if is a letter
+            ConsoleKeyInfo k when char.IsLetter(k.KeyChar) => k.KeyChar.ToString(),
+            // convert in char
+            ConsoleKeyInfo k when char.IsLetter(k.KeyChar) => k.KeyChar,
+            // if is not a char return the key
+            ConsoleKeyInfo k => k
+        };
+
+        // get an input from keyboard and return a bool or null
+        public static bool? GetArrow () => Console.ReadKey().Key switch
+        {
+            // when is up arrow return true
+            ConsoleKey k when k is ConsoleKey.UpArrow => true,
+            // when is down arrow return false
+            ConsoleKey k when k is ConsoleKey.DownArrow => false,
+            // else return null
+            _ => null
+        };
+
+        // get an input from keyboard and return a type
+        public static Type KeyType () => Console.ReadKey() switch
+        {
+            ConsoleKeyInfo k when char.IsNumber(k.KeyChar) => typeof(int),
+            ConsoleKeyInfo k when char.IsLetter(k.KeyChar) => typeof(char),
+            ConsoleKeyInfo k => k.Key.GetType(),
+        };
+    }
+
+
+    internal class Output
+    {
+
+        /// <summary> Print every parameter on console </summary>
+        public static void Log (params object[] args) => Console.WriteLine(string.Join(" ", args));
+
+        /// <summary> Change Console background color </summary>
+        /// <param name="color">color to set</param>
+        public static void ColorBG (ConsoleColor color) => Console.BackgroundColor = color;
+
+        /// <summary> Change Console text color </summary>
+        /// <param name="color">color to set</param>
+        public static void ColorTX (ConsoleColor color) => Console.ForegroundColor = color;
+
+        /// <summary> Change Console background and text color </summary>
+        /// <param name="back">background color to set</param>
+        /// <param name="fore">foreground color to set</param>
+        public static void Color (ConsoleColor back, ConsoleColor fore) { ColorBG(back); ColorTX(fore); }
+
+        /// <summary> Reset default console colors </summary>
+        public static void ColorReset () { ColorBG(ConsoleColor.Black); ColorTX(ConsoleColor.White); }
+    }
+}
---
+++ CSharp Version/Advent_Of_CSharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using static EasyCode.Output;
+using static EasyCode.Input;
+
+using dir = System.IO.Directory;
+
+
+
+void PrintError01()
+{
+    Color(ConsoleColor.Red, ConsoleColor.Black);
+    Log("Error 01: file not found. \n\nPress any key to exit.");
+    Console.ReadKey(); Environment.Exit(0);
+}
+
+
+
+// [ITA] finchè non trovi il file di input risali di un livello ma se esci da AdventOfCode stampa un errore
+// [ENG] until you find the input file go up one level but if you exit from AdventOfCode print an error
+while (!File.Exists("input.txt")) if (dir.Exists("AdventOfCode")) PrintError01(); else dir.SetCurrentDirectory("../");
+
+// [ITA] salva in un array le righe di testo presenti nel file trovato al percorso attuale
+// [ENG] save in an array the text lines present in the file found at the current path
+string[] lines = File.ReadAllLines(dir.GetCurrentDirectory() + "\\input.txt");
+
+
+
+
+
+
+// [ITA] scorri ogni riga del file di input
+// [ENG] scroll every line of the input file
+foreach (string line in lines)
+{
+    
+}
+
+
+Console.WriteLine("\n\nPress any key to exit.");
+Console.ReadKey();
